4906b4711deb3fb41444c5b2913e2662406bde82
JeisonLeonel <jeisonleonel0814@gmail.com>
2022-11-23 08:29:09 -0500

    Se realiza correcion en el js

--- public/js/agregar.js
+++ public/js/agregar.js
@@ -1,4 +1,4 @@
-let total_element = document.getElementById('total'); // obtenemos el elemento
+let total_element = document.getElementById('total'); 
 
 function sumar () {
   let total_value = parseInt(total_element.innerHTML);
@@ -8,11 +8,10 @@ function sumar () {
 
 function restar () {
   let total_value = parseInt(total_element.innerHTML);
-  if (total_value == 0) return; // validamos que el valor no sea menor a 0
+  if (total_value == 0) return;
   total_element.innerHTML = total_value - 1;
 }
 
-// asignamos funcion click a cada Div
 document.getElementById('mas').addEventListener('click', sumar);
 
 document.getElementById('menos').addEventListener('click', restar);
